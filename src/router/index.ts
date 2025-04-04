910c15a566b76d02b3f869d1880ac8264ac839f6
Roki K <kiroutp@yandex.ru>
2025-04-04 19:45:30 +0300

    change router on hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { useAuthStore } from "@/stores/auth";
 import Login from "@/components/Login.vue";
 import Dashboard from "@/components/Dashboard.vue";
 import NotFound from "@/components/NotFound.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
