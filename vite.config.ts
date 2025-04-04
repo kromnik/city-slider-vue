0c20513355aa5605d6b89a6095a2435f7682ac4e
Roki K <kiroutp@yandex.ru>
2025-04-04 17:30:29 +0300

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
-import { ghPages } from "vite-plugin-gh-pages";
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    ghPages(),
     // vueDevTools(),
   ],
   base: "/city-slider-vue/",
