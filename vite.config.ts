850c8567dbd0954016b608f0fe74bc16daac2a62
Roki K <kiroutp@yandex.ru>
2025-04-04 16:10:03 +0300

    addition vite.config.ts package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import { ghPages } from "vite-plugin-gh-pages";
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ghPages(),
     // vueDevTools(),
   ],
-  base: "/",
+  base: "/city-slider-vue/",
   server: {
     open: true,
   },
